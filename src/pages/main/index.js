2b3540df6f8c9a0a7c6faf5118d007f3cdd94f63
ranielOA <r.a_tw@hotmail.com>
2021-02-20 19:08:34 -0300

    no message

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import { StatusBar, View } from 'react-native';
+import { StatusBar } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 import Header from '../../components/Header';
 import Tabs from '../../components/Tabs';
-import { Container } from './styles';
+
+import {
+    Container,
+    Content,
+    Card,
+    CardHeader,
+    CardContent,
+    CardFooter,
+    Title,
+    Description,
+    Annotation,
+    SafeAreaView,
+} from './styles';
 
 const Main = () => {
     return (
@@ -11,6 +25,31 @@ const Main = () => {
 
             <Container>
                 <Header />
+
+                <Content>
+                    <Card>
+                        <CardHeader>
+                            <Icon name="attach-money" size={28} color="#666" />
+                            <Icon
+                                name="visibility-off"
+                                size={28}
+                                color="#666"
+                            />
+                        </CardHeader>
+
+                        <CardContent>
+                            <Title>Saldo disponível</Title>
+                            <Description>R$ 1.550.965,79</Description>
+                        </CardContent>
+
+                        <CardFooter>
+                            <Annotation>
+                                Transferência de R$ 30,00 recebida de Dom Pedro
+                                hoje às 08:00h
+                            </Annotation>
+                        </CardFooter>
+                    </Card>
+                </Content>
 
                 <Tabs />
             </Container>
