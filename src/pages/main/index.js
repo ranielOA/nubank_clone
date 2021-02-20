70870fdce74e63f95b9dabe873b4a429d8c8a1b8
ranielOA <r.a_tw@hotmail.com>
2021-02-20 18:39:04 -0300

    no message

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import {StatusBar, View} from 'react-native';
-import {Container} from './styles';
+import { StatusBar, View } from 'react-native';
+import Header from '../../components/Header';
+import Tabs from '../../components/Tabs';
+import { Container } from './styles';
 
 const Main = () => {
-  return (
-    <>
-      <StatusBar barStyle="light-content" backgroundColor="#8b10ae" />
-      <Container />
-    </>
-  );
+    return (
+        <>
+            <StatusBar barStyle="light-content" backgroundColor="#8b10ae" />
+
+            <Container>
+                <Header />
+
+                <Tabs />
+            </Container>
+        </>
+    );
 };
 
 export default Main;
